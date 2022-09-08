b2f27b0121859bcb2afeb225c8738b974db4ef31
Tabitht <imafidontabitha@gmail.com>
2022-09-08 18:56:23 +0100

    commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,20 @@
+	#include <stdio.h>
+/**
+ * main- to run the program
+ * void- to help retun 0
+ * Description: main helps to run the code)?
+(* section header:1)*
+ * Return: it will return 1
+ */
+	int main(void)
+{
+int i = 2015;
+int n = 10;
+int t = 19;
+
+printf("and that piece of art is useful%c %c", '"', '-');
+
+printf("Dora Korpar, %d%c%d%c%d", i, '-', n, '-', t);
+
+	return(1);
+}
