d48df3c9e0ec594399c1a88717a858438925a3fc
Tabitht <imafidontabitha@gmail.com>
2022-09-08 18:08:18 +0100

    new commit

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,7 +1,12 @@
 	#include <stdio.h>
 /**
- * main - to ensure the program runs
- * void- to return it to 0                             * Description: main ensure the program runs)?        (* section header: 1)*                                 * Return: to return 0                                 */
+ * main- to ensure the program runs
+ * void- to return it to 0
+ * Description: main ensures that it runs
+(* section header:1)*
+ * Return: to return 0
+ */
+/* sizeof is used to determine data size*/
 	int main(void)
 {
 char ch = sizeof(char);
@@ -10,15 +15,15 @@ long int li = sizeof(long int);
 long long int lli = sizeof(long long int);
 float f = sizeof(float);
 
-printf("size of a char:%2d\n" ,ch);
+printf("size of a char:%2d\n", ch);
 
-printf("size of an int:%2d\n" ,ch);
+printf("size of an int:%2d\n", i);
 
-printf("size of a long int:%2d\n" ,ch);
+printf("size of a long int:%2d\n", li);
 
-printf("size of a long long int:%2d\n" ,ch);
+printf("size of a long long int:%2d\n", lli);
 
-printf("size of a float:%2d\n" ,ch);
+printf("size of a float:%2d\n", f);
 
 	return (0);
 }
