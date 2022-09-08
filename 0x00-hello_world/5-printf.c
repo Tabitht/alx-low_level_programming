8b54a7deea6bf9cc4e12a399f5cd2af1f068bd1e
Tabitht <imafidontabitha@gmail.com>
2022-09-08 17:00:14 +0100

    new

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 /** main- to run the program
- * Description: the main causes program to run
+ * main Description: the main causes program to run
  * void- makes it to return 0
+ * Description: to print out some text
  * Return- to return 0
  */
+
 /* printf- prints out the text*/
 	int main(void)
 {
