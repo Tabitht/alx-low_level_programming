02f56bd23df549cee6870f5ca6979717161c6a14
Tabitht <imafidontabitha@gmail.com>
2022-09-08 16:31:12 +0100

    new commit

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,9 @@
-#include <stdio.h>
-/**                                                    * main- to beginning running the code                 * void- to return 0
- * Description:used to start running the code
- * Return: returns 0                                   */                                                           int main(void)
+#include <stdio.h>				      /** main- to beginning running the code               * void- to return 0
+* Description:used to start running the code
+* Return: returns 0            
+*/
+/*printf- to print out a number*/
+	int main(void)
 {
 printf("with proper grammar, but the outcome is a piece of art,\n");
 
