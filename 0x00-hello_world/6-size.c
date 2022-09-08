d69ae15811f86c78e0d9a1e9205c1f1854d3e55f
Tabitht <imafidontabitha@gmail.com>
2022-09-08 17:54:29 +0100

    successful commit

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,24 @@
+	#include <stdio.h>
+/**
+ * main - to ensure the program runs
+ * void- to return it to 0                             * Description: main ensure the program runs)?        (* section header: 1)*                                 * Return: to return 0                                 */
+	int main(void)
+{
+char ch = sizeof(char);
+int i = sizeof(int);
+long int li = sizeof(long int);
+long long int lli = sizeof(long long int);
+float f = sizeof(float);
+
+printf("size of a char:%2d\n" ,ch);
+
+printf("size of an int:%2d\n" ,ch);
+
+printf("size of a long int:%2d\n" ,ch);
+
+printf("size of a long long int:%2d\n" ,ch);
+
+printf("size of a float:%2d\n" ,ch);
+
+	return (0);
+}
