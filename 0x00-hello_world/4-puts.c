6a6e99e5ee5086bcb0c4abdd4712db75293b8153
Tabitht <imafidontabitha@gmail.com>
2022-09-08 15:37:24 +0100

    more corrections to file 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,13 @@
 	#include <stdio.h>
 /**
+ * main- to beginning running the code
+ * void- to return 0
  * Description:The main function is used to run the code to print out a string using puts.
+ * Return: returns 0
  */
 	int main(void)
 {
-	puts("Programming is like building a multilingual puzzle");
+puts("Programming is like building a multilingual puzzle");
 
 	return (0);
 }
