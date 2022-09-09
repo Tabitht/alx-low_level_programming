71e187eb61d0a135b2924cc076ddbd4ba04eebf0
Tabitht <imafidontabitha@gmail.com>
2022-09-09 21:09:32 +0000

    new file added

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -7,5 +7,9 @@
  */
 int main(void)
 {
+	int i, j;
+for (i = '0', j = '1'; i <= '9', j <= '9'; i++, j++)
+{
+i++j
 	return (0);
 }
--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -7,9 +7,16 @@
   */
 int main(void)
 {
-	char ch ;
-for (
+	char ch;
+for (ch = 'a'; ch <= 'z'; ch++)
 {
+if ((ch == 'q') || (ch == 'e'))
+continue;
+putchar(ch);
+}
+putchar('\n');
 
+	return (0);
+}
 
 
