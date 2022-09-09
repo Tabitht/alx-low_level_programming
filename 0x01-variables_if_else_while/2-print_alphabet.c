c68e4f0967e9a3155903aeb1f3d280667e3ab49d
Tabitht <imafidontabitha@gmail.com>
2022-09-09 14:03:25 +0100

    new file

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main- the entry point to enable the code run
+ *
+ *
+ * Return: will always return 0
+ */
+int main(void)
+{
+	char ch = 'a';
+while (ch <= 'z')
+{
+putchar(ch);
+ch++;
+}
+putchar(\n);
+	return (0);
+}
