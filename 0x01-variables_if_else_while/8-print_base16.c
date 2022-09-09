8faf76616e60a0d78708d6a3e27dac1b3c9ecf88
Tabitht <imafidontabitha@gmail.com>
2022-09-09 18:28:06 +0000

    added a file

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -8,5 +8,16 @@
 int main(void)
 {
 	int i;
-for (i =0; i < 16; i++)
+	char ch;
+for (i = '0'; i <= '9'; i++)
 {
+putchar(i);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+putchar('\n');
+
+	return (0);
+}
