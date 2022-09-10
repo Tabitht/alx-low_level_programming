88d6cc790dcdce851e8c229d5ed18abb8c754ed5
Tabitht <imafidontabitha@gmail.com>
2022-09-10 01:46:49 +0000

    new file addition

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -7,5 +7,27 @@
  */
 int main(void)
 {
-	return (0)
+	int j, k;
+for (j = 0; j <= 99; j++)
+{
+for (k = 1; k <= 99; k++)
+{
+if (j < k)
+{
+	putchar((j / 10) + '0');
+	putchar((j % 10) + '0');
+	putchar(' ');
+	putchar(k / 10 + '0');
+	putchar((k % 10) + '0');
+if (!(j == 98 && k == 99))
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+}
+}
+	putchar('\n');
+
+	return (0);
 }
