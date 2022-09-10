e3ad818f9d9e8a8594b567b3c4af715a5cebe454
Tabitht <imafidontabitha@gmail.com>
2022-09-10 00:45:35 +0000

    added a new file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -7,5 +7,34 @@
  */
 int main(void)
 {
+	int x;
+	int y;
+	int z = 0;
+while (z <= 9)
+{
+	y = 0;
+while (y <= 9)
+{
+	x = 0;
+while (x <= 9)
+{
+if ((z != y) && (y != x) && (z < y) && (y < x))
+{
+	putchar('0' + z);
+	putchar('0' + y);
+	putchar('0' + x);
+if (x + y + z != 9 + 8 + 7)
+{
+	putchar(',');
+	putchar(' ');
+}
+}
+x++;
+}
+y++;
+}
+z++;
+}
+putchar('\n');
 	return (0);
 }
