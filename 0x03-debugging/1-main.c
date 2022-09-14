d0ab71175f1fe30233010726544ce912fa694390
Tabitht <imafidontabitha@gmail.com>
2022-09-14 15:55:01 -0700

    corrections to file

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -11,13 +11,13 @@ int main(void)
 
 	printf("infinite loop incoming :(\n");
 	/**
- 	* i = 0;
- 	*
- 	* while (i < 10)
- 	* {
- 	* 	putchar(i);
- 	* }
- 	*/
+	*i = 0;
+	*
+	*while (i < 10)
+	*{
+	*putchar(i)
+	*}
+	*/
 	printf("Infinite loop avoided! \\0/\n");
 
 	return (0);
