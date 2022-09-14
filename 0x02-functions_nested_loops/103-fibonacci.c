c0e3aefde465656ae3e6787c5e8a6a65f065403b
Tabitht <imafidontabitha@gmail.com>
2022-09-14 06:12:31 -0700

    done

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,11 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main- entry point
+ * description: print the sum of even fibonacci to a fib value
+ * not exceeding 4,000,000
+ * Return: 0
+ */
+int main(void)
+{
+	int i= 1
