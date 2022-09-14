b1e829a31db02de75a6a79bb0843186cb1410ca0
Tabitht <imafidontabitha@gmail.com>
2022-09-14 04:49:50 +0100

    correction to files

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -11,13 +11,13 @@ void jack_bauer(void)
 for (k = 0; k <= 23; k++);
 {
 	for (j = 0; j <= 59; j++);
-{
-	_putchar((k / 10) + '0');
-	_putchar((k % 10) + '0');
-	_putchar(':');
-	_putchar((j / 10) + '0');
-	_putchar((j % 10) + '0');
-	_putchar('\n');
-}
+	{
+		_putchar((k / 10) + '0');
+		_putchar((k % 10) + '0');
+		_putchar(':');
+		_putchar((j / 10) + '0');
+		_putchar((j % 10) + '0');
+		_putchar('\n');
+	}
 }
 }
