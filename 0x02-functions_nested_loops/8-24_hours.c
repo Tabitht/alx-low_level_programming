f368c5768cd2e6889aa30ac54659931a3d63ad06
Tabitht <imafidontabitha@gmail.com>
2022-09-14 02:55:23 +0100

    corrected

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -9,7 +9,6 @@ void jack_bauer(void)
 {
 	int k, j;
 for (k = 0; k <= 23; k++);
-{
 for (j = 0; j <= 59; j++);
 {
 _putchar((k / 10) + '0');
@@ -18,6 +17,5 @@ _putchar(':');
 _putchar((j / 10) + '0');
 _putchar((j % 10) + '0');
 _putchar('\n');
-}
 }
 }
