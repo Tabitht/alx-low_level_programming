bba17e0f2b6f692917c1057585fd3ec2875b0a5d
Tabitht <imafidontabitha@gmail.com>
2022-09-19 21:47:19 +0100

    added new files

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -7,4 +7,25 @@
  */
 int _atoi(char *s)
 {
+	int m, n;
+	unsigned int o;
 
+	m = 1;
+	n = 0;
+	o = 0;
+	while (!(s[n] <= '9' && s[n] >= '0') && s[n] != '\0')
+	{
+		if(s[n] == '-')
+		{
+			m *= -1;
+			n++;
+		}
+	}
+	while (s[n] <= '9' && s[n] >= '0' && s[n] != '\0')
+	{
+		o = (o * 10) + (s[n] - '0');
+		n++;
+	}
+	o *= m;
+	return (o);
+}
