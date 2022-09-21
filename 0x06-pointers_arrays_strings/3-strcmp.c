0c0c84e0d6da83ee3818c8e3c501916643deada4
Tabitht <imafidontabitha@gmail.com>
2022-09-21 19:34:47 +0100

    corrected

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,15 +9,21 @@ int _strcmp(char *s1, char *s2)
 {
 	int i, j;
 
-	j = -1;
-	for (i = 0; s1[i] != '\0'; i++)
+	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
 	{
 		if (s1[i] == s2[i])
 		{
 			s1[i] = 0;
 			s2[i] = s1[i];
+			j = s2[i];
 		}
-		if (s1[i] > s2[i])
+	}
+	while (s1[i] == s2[i] && (s1[i] != '\0' || s2[i] != '\0'))
+	{
+		if (s1[i] > s2[i] || s1[i] < s2[i])
 		{
-			s1[i] - s2[i]
+			j = s1[i] - s2[i];
+		}
+	}
+return (j);
 }
