d19cb27c6b69718c8f4442963b159ab580db0522
Tabitht <imafidontabitha@gmail.com>
2022-09-27 04:47:10 +0100

    correction to file 5

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -10,25 +10,25 @@ char *_strstr(char *haystack, char *needle)
 {
 	int i, j, k, l;
 
-for (i = 0; haystack[i] != '\0'; i++)
-{
-}
-for (j = 0; needle[j] != '\0'; j++)
-{
-}
-for (k = 0; k < i; k++)
-{
-	if (haystack[k] == needle[0])
+	for (i = 0; haystack[i] != '\0'; i++)
 	{
-		for (l = 0; l < j; l++)
+	}
+	for (j = 0; needle[j] != '\0'; j++)
+	{
+	}
+	for (k = 0; k < i; k++)
+	{
+		if (haystack[k] == needle[0])
 		{
-			if (haystack[k + l] == needle[l])
-				return (haystack +k + l);
+			for (l = 0; l < j; l++)
+			{
+				if (haystack[k + l] == needle[l])
+					return (haystack +k + l);
+			}
 		}
 	}
-}
-		if (l == j)
-			return (needle);
+	if (l == j)
+		return (needle);
 	return (0);
 
 }
