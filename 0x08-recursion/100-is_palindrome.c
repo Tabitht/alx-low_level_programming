a6da6bd6f1d94e2b81df4bbc2da987c6fd6a1cd7
Tabitht <imafidontabitha@gmail.com>
2022-09-29 05:25:06 +0100

    new addition

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -1,4 +1,21 @@
 #include "main.h"
+#include <string.h>
+/**
+ * compare- to check if a string is palindrome
+ * @s: string to be tested
+ * @n: length of the string
+ * Return: returns 1 if true, 0 if false
+ */
+int compare(char *s, int n)
+{
+	if (n < 1)
+		return (1);
+	if (*s == *(s + n))
+	{
+		return (compare(s + 1, n - 2));
+	}
+	return (0);
+}
 /**
  * is_palindrome- to check if a string is palindr
  * ome i.e if a string when read backwards gives
@@ -9,6 +26,7 @@
  */
 int is_palindrome(char *s)
 {
+	int i = strlen(s);
 
-
+	return (compare (s, i - 1));
 }
