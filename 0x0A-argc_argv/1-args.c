3c21df2ad0773b0a2f041c802b9b55cf702e09eb
Tabitht <imafidontabitha@gmail.com>
2022-09-30 05:37:09 -0700

    corrected

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -2,9 +2,10 @@
 /**
  * main- entry point to run my code
  * @argc: to give number of arguments entered in command line
+ * @argv: arguments in command line
  * Return: returns 0
  */
-int main (int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	(void)argv;
 	printf("%d\n", argc - 1);
