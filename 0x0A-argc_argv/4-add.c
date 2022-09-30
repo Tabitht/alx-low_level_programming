33a76fbb240f2dff6a40b3810dd8681ebdd3a81b
Tabitht <imafidontabitha@gmail.com>
2022-09-30 20:19:55 +0100

    corrected

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -1,6 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 /**
+ * notinteger- to check if a given string is integer
+ * @s: string to check
+ * Return: returns an integer
+ */
+int notinteger(const char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] < '0' || s[i] > '9')
+			return (1);
+		i++;
+	}
+	return (0);
+}
+
+/**
  * main- entry point to run the code
  * @argc: the number of arguments supplied to command line
  * @argv: arguments supplied to command line
@@ -8,21 +26,18 @@
  */
 int main(int argc, char *argv[])
 {
-	int i;
 	int sum = 0;
 
-	for (i = 1; i < argc; i++)
+	while (--argc)
 	{
-		if (!(*argv++ >= 48 && *argv++ <= 57))
+		if (notinteger(argv[argc]))
 		{
 			printf("Error\n");
 			return (1);
 		}
 		else
-			sum += atoi(argv[i]);
+			sum += atoi(argv[argc]);
 	}
 	printf("%d\n", sum);
-	if (argc < 2)
-		printf("%d\n", 0);
 	return (0);
 }
