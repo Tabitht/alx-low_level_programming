951cad19076b50eb64700842b0011829ed17f970
Tabitht <imafidontabitha@gmail.com>
2022-10-03 20:44:53 +0100

    new file addition

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -10,26 +10,26 @@
 char *str_concat(char *s1, char *s2)
 {
 	char *ptr;
-	int i, j, k= 0;
+	int i, j, k, L;
 
-	for (i = 0; s1[i] != '\0' || s2[i] != '\0'; i++)
+	if (s1 == NULL || s2 == NULL)
+		return (" ");
+	for (i = 0; s1[i] != '\0'; i++)
 		;
-	for (j = 0; s1[j] != '\0'; j++)
+	for (j = 0; s2[j] != '\0'; j++)
 		;
 
-	ptr = malloc(sizeof(char) * i);
+	ptr = malloc(sizeof(char) * (i + j + 1));
 	if (ptr != NULL)
 	{
-		for (k = 0; s2[k] != '\0'; k++)
-		{
-			s1[j] = s2[k];
-		}
-			s1[j] = '\0';
+		for (k = 0; (ptr[k] = s1[k]) != '\0'; k++)
+			;
+		for (L = 0; (ptr[L] = s2[L]) != '\0'; L++)
+			;
+		ptr[L] = '\0';
 	}
 	else
 		return (NULL);
-	if (s1 == NULL || s2 == NULL)
-		return (" ");
 	return (ptr);
 }
 
