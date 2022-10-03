255936c817670f213860280255ccb4f5d5e138d9
Tabitht <imafidontabitha@gmail.com>
2022-10-03 23:34:44 +0100

    new file addition

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -15,11 +15,11 @@ int **alloc_grid(int width, int height)
 	if (width <= 0 || height <= 0)
 		return (NULL);
 
-	ptr = (int **) malloc(sizeof(int *) * width);
+	ptr = (int **) malloc(sizeof(int *) * height);
 	if (ptr == NULL)
 		return (NULL);
-	for (i = 0; i < width; i++)
-		ptr[i] = (int *) malloc(sizeof(int) * height);
+	for (i = 0; i < height; i++)
+		ptr[i] = (int *) malloc(sizeof(int) * width);
 	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
