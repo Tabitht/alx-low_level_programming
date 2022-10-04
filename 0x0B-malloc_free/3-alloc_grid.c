dc9dfbbbe8d1ce54bab00939dba46ea1f22c9f72
Tabitht <imafidontabitha@gmail.com>
2022-10-04 04:00:10 +0100

    new file addition

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -29,8 +29,8 @@ int **alloc_grid(int width, int height)
 		for (j = 0; j < width; j++)
 			ptr[i][j] = 0;
 	}
+	return (ptr);
 	for (i = 0; i < height; i++)
 		free(ptr[i]);
 	free(ptr);
-	return (ptr);
 }
