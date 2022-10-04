3bf094b9827c8e3075c4431f782d29c7deabca25
Tabitht <imafidontabitha@gmail.com>
2022-10-04 11:58:31 +0100

    new file malloc

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <string.h>
+
+/**
+ * argstostr- to concatenate all the argument of tge program
+ * @ac: number of arguments
+ * @av: arguments of the program
+ * Return: returns a char
+ */
+char *argstostr(int ac, char **av)
+{
+	int i, k, j = 0;
+	char *ptr;
+
+	if (ac == 0 || av == NULL )
+		return (NULL);
+	for (i = 0; i < ac; i++)
+	{
+		j += strlen(av[i]);
+	}
+	ptr = malloc(sizeof(char) * j + 1);
+	if (ptr != NULL)
+	{
+		for (i = 0; i < ac; i++)
+		{
+			for (k = 0; av[i][k] != '\0'; k++)
+			{
+				ptr[j] = av[i][k];
+				j++;
+			}
+			ptr[j] = '\n';
+		}
+		ptr[j] = '\0';
+		return (ptr);
+	}
+	else
+		return (NULL);
+}
+
+
+
+
+
+
+
+
