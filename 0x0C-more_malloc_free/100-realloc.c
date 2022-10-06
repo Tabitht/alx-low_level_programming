ed028f54c758080c8f2d6335ab6f6822922dcd4f
Tabitht <imafidontabitha@gmail.com>
2022-10-07 00:44:47 +0100

    new addition

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -13,21 +13,19 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
 	void *ptr2;
 
-	if (new_size == old_size)
-		return (ptr);
-	ptr2 = malloc(new_size);
-	if (ptr == NULL)
-	{
-		return (malloc(new_size));
-	}
-	if (new_size > old_size)
-	{
-		memcpy(ptr2, ptr, old_size);
-	}
 	if (new_size == 0 && ptr != NULL)
 	{
 		free(ptr);
 		return (NULL);
+	}
+	if (ptr == NULL)
+		return (malloc(new_size));
+	if (new_size == old_size)
+		return (ptr);
+	ptr2 = malloc(new_size);
+	if (new_size > old_size)
+	{
+		memcpy(ptr2, ptr, old_size);
 	}
 	return (ptr2);
 }
