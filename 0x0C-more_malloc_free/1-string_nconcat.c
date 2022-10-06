c39ba011dc59930ed9d06c5b6a05520a3b9d7609
Tabitht <imafidontabitha@gmail.com>
2022-10-06 16:11:23 +0100

    more source files on malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -20,7 +20,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ("");
 	if (n >= strlen(s2))
 		n = strlen(s2);
-	ptr = malloc(sizeof(*ptr) * (strlen(s1) + n + 1));
+	ptr = malloc(sizeof(char) * (strlen(s1) + n + 1));
 	if (ptr == NULL)
 		return (NULL);
 	for (a = 0; a < strlen(s1); a++, c++)
