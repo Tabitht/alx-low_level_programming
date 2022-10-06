66a0f0b2035161f5d4f7b73a5273fc92dce2cbe5
Tabitht <imafidontabitha@gmail.com>
2022-10-06 16:07:03 +0100

    more source files on malloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,7 +9,7 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	void *ptr;
+	int *ptr;
 	unsigned int i;
 
 	if (nmemb == 0 || size == 0)
@@ -19,7 +19,7 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 		return (NULL);
 	for (i = 0; i < nmemb; i++)
 	{
-		ptr[i] = 0;
+		 ptr[i] = 0;
 	}
 	return (ptr);
 }
