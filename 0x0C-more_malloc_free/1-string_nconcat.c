627fb54b476846da7e7e73dfe1ebd1d6940e4223
Tabitht <imafidontabitha@gmail.com>
2022-10-06 13:45:15 +0100

    new addition still on more malloc

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -18,7 +18,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		return ("");
 	if (s2 == NULL)
 		return ("");
-	ptr = malloc(sizeof (*ptr) * (strlen(s1) + n + 1));
+	ptr = malloc(sizeof(*ptr) * (strlen(s1) + n + 1));
 		if (ptr == NULL)
 			return (NULL);
 	for (a = 0; a < strlen(s1); a++, c++)
