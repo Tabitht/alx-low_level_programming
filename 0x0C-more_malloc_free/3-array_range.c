6400ce009828559eaf4539a1a3f6f3864b92f873
Tabitht <imafidontabitha@gmail.com>
2022-10-06 15:53:45 +0100

    more source files on malloc

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -2,6 +2,23 @@
 #include <stdlib.h>
 #include "main.h"
 /**
+ * array_length- to get the legth of a given array
+ * @min: the first element of array
+ * @max: the last element of the array
+ * Return: returns the length of the array
+ */
+int array_length(int min, int max)
+{
+	int i = 0;
+
+	while (min <= max)
+	{
+		i++;
+		min++;
+	}
+	return (i);
+}
+/**
  * array_range- to create an array of integers
  * @min: the minimum range of integer in array
  * @max: the maximum range of integer in array
@@ -9,4 +26,20 @@
  */
 int *array_range(int min, int max)
 {
+	int *ptr;
+	int i, j = 0;
+
+	if (min > max)
+		return(NULL);
+	i = array_length(min, max);
+	ptr = malloc(sizeof(int) * i);
+	if (ptr == NULL)
+		return (NULL);
+	while (i > 0)
+	{
+		ptr[j] = min;
+		min += 1;
+		i--;
+	}
+	return (ptr);
 }
