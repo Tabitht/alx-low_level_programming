89f12ba93b3cf8fc5649bf87fdbaa10d26f6e097
Tabitht <imafidontabitha@gmail.com>
2022-10-10 20:02:11 +0100

    correction made

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -1,14 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "dog.h"
+
 /**
  * print_dog- to print the contents of struct dog
  * @d: variable to access member of the structure
  * Return: void
  */
+
 void print_dog(struct dog *d)
 {
-if (d != NULL)
+if (!(d == NULL))
 {
 	if (d->name == NULL)
 		d->name = "(nil)";
