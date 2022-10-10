06896b1cc07ad38d0b653ee2e90e19a031a5173d
Tabitht <imafidontabitha@gmail.com>
2022-10-10 16:54:03 +0100

    new file on structures

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+/**
+ * init_dog - a function that initializes a variable of type struct dog
+ * @d: the structure variable
+ * @name: first memeber of char datatype, initializes to a name
+ * @age: second member of float datatype, intializes to age
+ * @owner: it initializes to the owner of the dog
+ */
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	d -> name = name;
+	d -> age = age;
+	d -> owner = owner;
+}
+
---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include "dog.h"
+#include <stdio.h>
+/**
+ * free_dog- to free previously allocated space
+ * @d: variable of dog_t
+ * Return: void
+ */
+void free_dog(dog_t *d)
+{
+	free(d -> name);
+	free(d -> owner);
+	free(d);
+}
