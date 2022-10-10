d82cdc6c4ed5c3e501ad48e11e9db319b4dc1106
Tabitht <imafidontabitha@gmail.com>
2022-10-10 19:19:26 +0100

    more corrections

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -26,13 +26,13 @@ dog_t *new_dog(char *name, float age, char *owner)
 	d->name = malloc(sizeof(char) * i + 1);
 	if ((d->name) == NULL)
 	{
-		free(d->name);
+		free(d);
 		return (NULL);
 	}
 	d->owner = malloc(sizeof(char) * j + 1);
 	if ((d->owner) == NULL)
 	{
-		free(d->owner);
+		free(d->name);
 		free(d);
 		return (NULL);
 	}
