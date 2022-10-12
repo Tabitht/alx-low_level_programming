e4c7234802edff7c89a532b047834b8c28c00448
Tabitht <imafidontabitha@gmail.com>
2022-10-12 20:36:53 +0100

    new file on function pointers

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,22 +10,25 @@
 int main(int argc, char *argv[])
 {
 	int num1, num2, result;
-	int (*op)(int, int);
 
 	if (argc != 4)
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	op = get_op_func(argv[2]);
-	if (!op)
+	if (strlen(argv[2]) != 1)
+	{
+		printf("Error\n");
+		exit(99);
+	}
+	if (!(get_op_func(argv[2])))
 	{
 		printf("Error\n");
 		exit(99);
 	}
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
-	result = op(num1, num2);
+	result = (*get_op_func(argv[2]))(num1, num2);
 	printf("%d\n", result);
 	return (0);
 }
