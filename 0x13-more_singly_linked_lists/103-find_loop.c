8b4e415a7e62a6ec5160454f95b3b53b20137fc2
Tabitht <imafidontabitha@gmail.com>
2022-10-25 21:47:36 +0100

    new file addition still on linked list

--- 0x13-more_singly_linked_lists/103-find_loop.c
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -19,9 +19,9 @@ listint_t *find_listint_loop(listint_t *head)
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-			return (fast);
+			return (slow);
 		else
 			return (NULL);
 	}
-	return (fast);
+	return (slow);
 }
