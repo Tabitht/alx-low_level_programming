047bbd186d6898a07189e697fd9a14dc9ef3fba9
Tabitht <imafidontabitha@gmail.com>
2022-10-25 21:38:26 +0100

    new file addition still on linked list

---
+++ 0x13-more_singly_linked_lists/103-find_loop.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+/**
+ * find_listsint_loop- finds the loop in a linked list
+ * @head: pointer to the linked list
+ * Return: address of the node where the loop starts or null if there is no loop
+ */
+listint_t *find_listint_loop(listint_t *head)
+{
+	listint_t *slow, *fast;
+	slow = head;
+	fast = head;
+
+	if (head == NULL)
+		return (NULL);
+	while (slow && fast && fast->next)
+	{
+		slow = slow->next;
+		fast = fast->next->next;
+		if (slow == fast)
+			return (fast);
+		else
+			return (NULL);
+	}
+	return (fast);
+}
