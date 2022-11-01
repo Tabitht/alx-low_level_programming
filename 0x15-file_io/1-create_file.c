5be382e5fbe8bb372f0416a65c790674213019ac
Tabitht <imafidontabitha@gmail.com>
2022-11-01 05:25:26 +0100

    new file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -7,4 +7,21 @@
  */
 int create_file(const char *filename, char *text_content)
 {
+	int o, w, i;
+
+	if (filename == NULL)
+		return (-1);
+	o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+	if (o < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		w = write(o, text_content, i);
+		if (w != i)
+			return (-1);
+	}
+	close(o);
+	return (1);
 }
