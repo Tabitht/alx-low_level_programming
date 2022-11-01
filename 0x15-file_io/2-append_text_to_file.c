1d5656c985ea2b887c7a614ffb25380fc1d9bdd2
Tabitht <imafidontabitha@gmail.com>
2022-11-01 05:36:39 +0100

    new file

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -9,4 +9,21 @@
 
 int append_text_to_file(const char *filename, char *text_content)
 {
+	int o, w, i = 0;
+
+	if (filename == NULL)
+		return (-1);
+	o = open(filename, O_WRONLY | O_APPEND);
+	if (o < 0)
+		return (-1);
+	if (text_content)
+	{
+		while (text_content[i])
+			i++;
+		w = write(o, text_content, i);
+		if (w != i)
+			return (-1);
+	}
+	close(o);
+	return (1);
 }
