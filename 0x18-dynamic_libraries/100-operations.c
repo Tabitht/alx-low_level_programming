9281119318e43107b6d320210bf3f7c7defdefbd
Tabitht <imafidontabitha@gmail.com>
2022-12-13 04:18:39 +0100

    more files on dynamic library

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,50 @@
+/**
+  * add - addition
+  * @a: number
+  * @b: number
+  * Return: result
+  */
+int add(int a, int b)
+{
+	return (a + b);
+}
+/**
+  * sub - subtraction
+  * @a: number to be subtracted from
+  * @b: number to subtract
+  * Return: subtaction of a and b
+  */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+  * mul - multiplication
+  * @a: number to multiply
+  * @b: number to multiply
+  * Return: multiplication of a and b
+  */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+  * div - division
+  * @a: number to divide
+  * @b: number to be divided with
+  * Return: division of a and b
+  */
+int div(int a, int b)
+{
+	return (a/ b);
+}
+/**
+  * mod - modulus
+  * @a: number to get reminder from
+  * @b: number to divide with
+  * Return: modulus of a and b
+  */
+int mod(int a, int b)
+{
+	return (a % b);
+}
